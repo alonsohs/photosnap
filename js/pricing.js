eae8eda710ea68f9351b87d54df2f8728e0d5d52
Alonso Hernández Sastré <alonsohdez14@gmail.com>
2020-11-20 12:42:35 -0600

    Pug/HTML structure and Desktop view styles done

---
+++ js/pricing.js
@@ -0,0 +1,46 @@
+const switchButton = document.getElementById("switch-pricing")
+const switchLabels = document.getElementsByClassName("toogle__plan")
+const pricingPriceLabels = document.getElementsByClassName("pricing__price")
+const pricingPerLabels = document.getElementsByClassName("pricing__per")
+
+const yearlyPrices = ['190', '390', '990']
+const monthlyPrices = ['19', '39', '99']
+
+switchButton.addEventListener("click", () => {
+    if (switchButton.checked)
+        setYearly()
+    else
+        setMonthly()
+})
+
+const setMonthly = () => {
+    for (let i = 0; i < switchLabels.length; i++) {
+        switchLabels[i].classList.toggle("selected")
+    }
+
+    setMonthlyPrices()
+}
+
+const setYearly = () => {
+    for (let i = 0; i < switchLabels.length; i++) {
+        switchLabels[i].classList.toggle("selected")
+    }
+
+    setYearlyPrices()
+}
+
+const setMonthlyPrices = () => {
+    for (let i = 0; i < pricingPriceLabels.length; i++) {
+        pricingPriceLabels[i].textContent = `$${monthlyPrices[i]}.00`
+        pricingPerLabels[i].textContent = `per month`
+    }
+}
+
+const setYearlyPrices = () => {
+    for (let i = 0; i < pricingPriceLabels.length; i++) {
+        pricingPriceLabels[i].textContent = `$${yearlyPrices[i]}.00`
+        pricingPerLabels[i].textContent = `per year`
+    }
+}
+
+setMonthlyPrices()
