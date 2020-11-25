e9c08d5fb45037e76b8366a03f9d70a3d4d7e707
Alonso Hernández Sastré <alonsohdez14@gmail.com>
2020-11-25 08:24:12 -0600

    menu bug fixed #1

--- js/menu.js
+++ js/menu.js
@@ -5,7 +5,7 @@ const burgerButtonImage = document.querySelector("#header_bg--img")
 burgerButton.addEventListener('click', () => {
     menu.classList.toggle("active")
     if (menu.classList.contains("active"))
-        burgerButtonImage.src = "../assets/shared/mobile/close.svg"
+        burgerButtonImage.src = "./assets/shared/mobile/close.svg"
     else
-        burgerButtonImage.src = "../assets/shared/mobile/menu.svg"
+        burgerButtonImage.src = "./assets/shared/mobile/menu.svg"
 })
